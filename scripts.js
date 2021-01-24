75e5704356b50185cabd353564fd8a4e4a76e2ca
jmolina518 <jmolina518@gmail.com>
2021-01-23 20:47:18 -0600

    add click eventListener

--- scripts.js
+++ scripts.js
@@ -4,8 +4,15 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
 // Build out functions
 
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
 //Hook up the event listeners
+video.addEventListener('click', togglePlay);
