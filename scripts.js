9c6663eb3bb9d07b790c8be51bbcd99e18b3273f
jmolina518 <jmolina518@gmail.com>
2021-01-23 21:46:04 -0600

    add scrub function

--- scripts.js
+++ scripts.js
@@ -32,6 +32,11 @@ function handleProgress() {
   progressBar.style.flexBasis = `${percent}%`;
 }
 
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 //Hook up the event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
@@ -44,3 +49,4 @@ ranges.forEach((range) => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach((range) =>
   range.addEventListener('mousemove', handleRangeUpdate)
 );
+progress.addEventListener('click', scrub);
